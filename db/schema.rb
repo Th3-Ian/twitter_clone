e0264003db062d99a830ddd4ecd104f91d4ad9c6
Ian Hart <ianhart13@gmail.com>
2021-05-25 08:55:51 -0400

    updated controller, models for logged in/logged out functionality, connected tweeets with users

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_191237) do
+ActiveRecord::Schema.define(version: 2021_05_25_002416) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
